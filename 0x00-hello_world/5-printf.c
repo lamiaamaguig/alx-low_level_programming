4297ffb70c1a4305474845fcfe2fcf3f767ab61e
lamiaamaguig <lamiaamaguig71@gmail.com>
2023-02-16 17:42:54 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - a programme that prints a line followed by a nw line
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
