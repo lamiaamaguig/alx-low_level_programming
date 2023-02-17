15b7f0df1031eadebd4fa9c5f643b48c53a50d24
lamiaamaguig <lamiaamaguig71@gmail.com>
2023-02-16 20:03:59 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,9 +11,5 @@ printf("Size of an int: %zu byte(s)\n", sizeof(int));
 printf("Size of a long int: %zu bytes(s)\n", sizeof(long int));
 printf("Size of a long long int: %zu bytes(s)\n", sizeof(long long int));
 printf("Size of a float: %zu bytes(s)\n", sizeof(float));
-return (0);
-char str[147]="your 147 character string here";
-char newstr[144];
-memcpy(newstr,str,144};
-		newstr[144]='\O';
+return (0};
 		}
