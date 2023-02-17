38fb390976929b10abff9abdda19351f868969c9
lamiaamaguig <lamiaamaguig71@gmail.com>
2023-02-17 14:53:24 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,4 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <time.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
 /**
  * main - Entry  point
  * Return: Always 0 (success)
