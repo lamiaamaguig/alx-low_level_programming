ff4df13400f8429663c53458207a0772a568e968
lamiaamaguig <lamiaamaguig71@gmail.com>
2023-02-17 14:48:03 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * main - Entry  point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char c;
+c = 'a';
+while
+	(c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+putchar("\n");
+return (0);
+}
