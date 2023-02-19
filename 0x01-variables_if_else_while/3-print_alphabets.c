383cd76330a759a7fef33637d8564ca1ad84841c
Your Name <you@example.com>
2023-02-19 13:49:47 -0800

    git config --global user.name "Your Name"

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Return Always 0 (success)
+ * main - prints the alphabet in lowercase,
+ * followed by a new line, except q and e
+ * Return: Aways 0 (Sucess)
  */
 int main(void)
 {
-	char c;
-	char d;
-	c = 'a';
-	d = 'A';
-	while
-		(c <= 'z') {
-			putchar(c);
-			c++;
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
 		}
-	while
-		(d <= 'Z') {
-			putchar(c);
-			d++;
-		}
+		ch++;
+	}
 	putchar('\n');
 	return (0);
 }
