aabaf6c3c5376d5d9a3c3f226ac596954d860d2a
lamiaamaguig <lamiaamaguig71@gmail.com>
2023-02-22 13:53:36 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,13 +8,14 @@ void print_alphabet_x10(void)
 	char c;
 	int i = 0;
 
-	while (i <= 0)
+	while (i < 10)
 	{
 		for (c = 'a'; c <= 'z' ; c++)
 		{
 			_putchar(c);
+		}
 			_putchar('\n');
 			i++;
 		}
-	}
+
 }
