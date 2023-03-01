f246a7e774ce6e021c60210d50ec2b33774e9119
lamiaamaguig <lamiaamaguig71@gmail.com>
2023-03-01 08:25:25 -0800

    #include "main.h" /**  * print_last_digit - prints the last digit of a number  *  * @n: the int to extract the last digit from  *  * Return: value of the last digit  *  */ int print_last_digit(int n) { int a;
    if (n < 0)
    n = -n;
    
    a = n % 10;
    
    if (a < 0)
    a = -a;
    
    _putchar(a + 0);
    
    return (a);
    }

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,18 +1,25 @@
 #include "main.h"
 /**
-* print_last_digit - prints the last digit of a number
-* @c: the number to be computed.
-* Return: value of the last digit of number
-*/
-int print_last_digit(int c)
+ * print_last_digit - prints the last digit of a number
+ *
+ * @n: the int to extract the last digit from
+ *
+ * Return: value of the last digit
+ *
+ */
+int print_last_digit(int n)
 {
-int last;
-{
-last = n % 10;
-if (last < 0)
-{
-last = last * -1;
+	int a;
+
+	if (n < 0)
+		n = -n;
+
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
+
+	return (a);
 }
-_putchar(last + '0');
-return (last);
-} 
