224f57fc5fe84b3934b371bc76e147559f6699d8
lamiaamaguig <lamiaamaguig71@gmail.com>
2023-03-21 15:27:07 -0700

    lolo

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints name of file it is complied from
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
