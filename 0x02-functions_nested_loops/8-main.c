17c7cbde6896eda1e53786a446d96a0b43874d9a
Your Name <you@example.com>
2023-05-01 16:53:29 -0700

    printf

--- 0x02-functions_nested_loops/8-main.c
+++ 0x02-functions_nested_loops/8-main.c
@@ -3,10 +3,12 @@
 /**
  * main - check the code
  *
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ *
  * Return: Always 0.
  */
 int main(void)
 {
-    jack_bauer();
-    return (0);
+	jack_bauer();
+	return (0);
 }
