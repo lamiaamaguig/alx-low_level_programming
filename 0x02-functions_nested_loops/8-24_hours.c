ce6d1ab1793857333ee9a806c7fc64efca347338
Your Name <you@example.com>
2023-05-01 18:03:04 -0700

    lolo

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * jack_baver - prints every minute of the day of Jack Bauer
+ * jack_bauer - prints every minute of the day of Jack Bauer
  *
  * starting from 00:00 to 23:59
  *
  */
-void jack_baver(void)
+void jack_bauer(void)
 {
 	int i, j;
 
